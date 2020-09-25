e2713b270f7aacf27b6b6a35aff2377134e1be66
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-09-25 15:37:37 +0200

    WIP

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,23 @@
 
 
-functionrender_slides <- function(dir){
-  lapply(list.files(dir, pattern = '.Rmd', 
-             full.names = TRUE), rmarkdown::render)
-  if( file.exists(file.path(dir,'render')) ){
-    system(glue::glue('rm -rf {{file.path(dir,"render")}}'))
+functionrender_slides <- function(directory){
+  brows
+  if( file.exists(file.path(directory,'render')) ){
+    system(glue::glue('rm -rf {file.path(directory,"render")}'))
   } else {
-    system(glue::glue('mkdir {{file.path(dir,"render"")}}'))
+    system(glue::glue('mkdir {file.path(directory,"render")}'))
   }
+  lapply(list.files(directory, pattern = '.Rmd', 
+             full.names = TRUE), rmarkdown::render)
+  lapply(list.files(directory,  
+                    recursive = TRUE), function(file_){
+                      if(file_ != 'render')
+                        file.copy(file.path(directory, file_),  
+                                  to = file.path(directory, "render"),
+                                  recursive=TRUE)
+                    }
+  )
+  
 }
 
 
