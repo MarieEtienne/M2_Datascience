69a1997b7aa130e46f3d1ad3486cea37b48bb2d4
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-09-27 23:00:10 +0200

    Version with render directory

--- R/utils.R
+++ R/utils.R
@@ -1,24 +1,12 @@
 
 
 functionrender_slides <- function(directory){
-  brows
-  if( file.exists(file.path(directory,'render')) ){
-    system(glue::glue('rm -rf {file.path(directory,"render")}'))
-  } else {
-    system(glue::glue('mkdir {file.path(directory,"render")}'))
-  }
+  system('mkdir -p render')
   lapply(list.files(directory, pattern = '.Rmd', 
              full.names = TRUE), rmarkdown::render)
-  lapply(list.files(directory,  
-                    recursive = TRUE), function(file_){
-                      if(file_ != 'render')
-                        file.copy(file.path(directory, file_),  
-                                  to = file.path(directory, "render"),
-                                  recursive=TRUE)
-                    }
-  )
-  
+  system( glue::glue( 'cp -rf  {directory} render/ '))
 }
 
 
+functionrender_slides('Visualisation')
 
