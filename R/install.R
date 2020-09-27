c3649a03e700963ca56b9bfc57701cf2c03efb4b
MarieEtienne <MarieEtienne@users.noreply.github.com>
2020-09-27 20:23:37 +0000

    deploy: 1c34ac627c99f9f3747ee4ca674075fb4c88f5f5

---
+++ R/install.R
@@ -0,0 +1,59 @@
+##########################################################
+#### cran packages
+##########################################################
+
+
+# system ------------------------------------------------------------------
+install.packages('remotes')
+install.packages('devtools')
+
+# tidyverse ---------------------------------------------------------------
+install.packages('tidyverse')
+
+# graphic -----------------------------------------------------------------
+install.packages('plotly')
+install.packages('ggpubr')
+# ## sudo apt-get install cargo 
+# install.packages('gifski') 
+# ## sudo add-apt-repository -y ppa:cran/imagemagick
+# ## sudo apt-get update
+# ## sudo apt-get install -y libmagick++-dev
+# install.package('magick')   
+# install.packages('gganimate')
+# install.packages('GGally')
+# 
+# # color -------------------------------------------------------------------
+# install.packages('wesanderson')
+# 
+# # maps --------------------------------------------------------------------
+# install.packages('sf')
+# install.packages('ggmap')
+# install.packages('rnaturalearth')
+# install.packages('rnaturalearthdata')
+# install.packages('rgeos')
+# 
+# # reporting ---------------------------------------------------------------
+# install.packages('rmarkdown')
+# install.packages('kableExtra')
+# install.packages('RefManageR')
+
+# 
+# # presentation ------------------------------------------------------------
+# 
+# 
+# 
+# ##########################################################
+# #### GitHub package
+# ##########################################################
+# 
+# 
+# # data --------------------------------------------------------------------
+# remotes::install_github("allisonhorst/palmerpenguins")
+# 
+# # presentation ------------------------------------------------------------
+# remotes::install_github('yihui/xaringan')
+# remotes::install_github("EvaMaeRey/flipbookr")
+# 
+# # map ---------------------------------------------------------------------
+# remotes::install_github("ropensci/osmdata")
+# # 
---
+++ README.md
@@ -0,0 +1,5 @@
+Reproducible science also means reproducible teaching. This repo is an attempt to produce teaching materials in a more consistant way.
+
+# Visualisation
+The first course developped for this series. First used in the Rennes R group on September 2020.
+
