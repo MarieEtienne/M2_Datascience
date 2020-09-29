b188e43f0d092048f706206575c712badc5185e1
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-09-29 10:02:04 +0200

    Store config files in resources

--- R/render.R
+++ R/render.R
@@ -1,4 +1,4 @@
-system('rm -rf render')
+#system('rm -rf render')
 system('mkdir -p render')
 dirnames <- stringr::str_split(
   list.files('.', pattern = '.Rmd',
@@ -9,9 +9,12 @@ dirnames <- stringr::str_split(
 for(directory in dirnames){
   filenames <- list.files(directory, pattern = '.Rmd',
                           full.names = TRUE)
+  system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
+  
   for(f_ in filenames){
     system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_cache'))
     system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_files'))
+    
     rmarkdown::render(f_)
   }
   system( glue::glue( 'cp -rf  {directory} render/ '))
