8037158f6038c78e4bd9baaa66f26c24b9f1ef1d
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-11-01 17:48:18 +0100

    Cours Visu Part1

--- R/render_local.R
+++ R/render_local.R
@@ -1,4 +1,4 @@
-directory <- 'linearmodel'
+directory <- 'M2_Dataviz'
 filenames <- list.files(directory, pattern = 'Rmd',
                           full.names = TRUE)
 
@@ -7,7 +7,6 @@ system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
 for(f_ in filenames){
   system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_cache'))
   system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_files'))
-  
   rmarkdown::render(f_)
 }
 
