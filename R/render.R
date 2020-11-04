4628829152f8d77cc9ce45bafc767eda5b8dcf13
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-11-04 12:15:18 +0100

    WIP test action render

--- R/render.R
+++ R/render.R
@@ -1,22 +1,26 @@
-#system('rm -rf render')
 system('mkdir -p render')
-dirnames <- stringr::str_split(
-  list.files('.', pattern = '.Rmd',
-             full.names = TRUE, recursive = TRUE) ,
-  pattern = '/', simplify = TRUE)[,2]
-
-
-for(directory in dirnames){
-  filenames <- list.files(directory, pattern = '.Rmd',
-                          full.names = TRUE)
-  system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
-  
-  for(f_ in filenames){
-    system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_cache'))
-    system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_files'))
-    
-    rmarkdown::render(f_)
-  }
-  system( glue::glue( 'cp -rf  {directory} render/ '))
-  
+dirnames <- 'Visualisation/'
+if(is.null(directory)){
+  system(glue::glue( 'rm -rf render'))
+  dirnames <- list.files('.', pattern = 'Rmd',
+                          full.names = TRUE, recursive = TRUE) %>% 
+    stringr::str_extract( pattern = './\\w+/') %>% 
+    str_replace_all(pattern = '\\./|/', replacement = '') %>% 
+    unique() 
 }
+
+
+
+for(d_ in dirnames)
+  system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
+filenames <- list.files(dirnames, pattern = 'Rmd',
+                        full.names = TRUE, recursive = TRUE)
+
+for(f_ in filenames){
+  system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_cache'))
+  system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_files'))
+  
+  rmarkdown::render(f_)
+}
+system( glue::glue( 'cp -rf  {directory} render/ '))
+
