f14d915f9f60ab00ddbafaa5687d3d3d702ad9b8
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-11-05 17:15:50 +0100

    Test dev version

---
+++ R/render_dev.R
@@ -0,0 +1,17 @@
+#system('rm -rf render')
+system('mkdir -p render')
+directory <- 'M2_Dataviz'
+
+
+  filenames <- list.files(directory, pattern = '.Rmd',
+                          full.names = TRUE)
+  system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
+  
+  for(f_ in filenames){
+    system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_cache'))
+    system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_files'))
+    
+    rmarkdown::render(f_)
+  }
+  system( glue::glue( 'cp -rf  {directory} render/ '))
+  
