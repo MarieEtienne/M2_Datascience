e907ffa9a387fceea1edb54e54ccdf19da64f77d
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2020-11-23 00:05:47 +0100

    Dashoboard example

--- R/render_dev.R
+++ R/render_dev.R
@@ -1,12 +1,8 @@
 #system('rm -rf render')
 system('mkdir -p render')
-dirnames <- stringr::str_split(
-  list.files('.', pattern = '.Rmd',
-             full.names = TRUE, recursive = TRUE) ,
-  pattern = '/', simplify = TRUE)[,2]
+directory <- 'M2_Dataviz'
 
 
-for(directory in dirnames){
   filenames <- list.files(directory, pattern = '.Rmd',
                           full.names = TRUE)
   system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
@@ -19,4 +15,3 @@ for(directory in dirnames){
   }
   system( glue::glue( 'cp -rf  {directory} render/ '))
   
-}
